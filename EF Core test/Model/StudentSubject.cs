17190ad9f767f36aca80b8768c01e56da4a76f43
Badralbadawi <61519595+Badralbadawi@users.noreply.github.com>
2023-05-09 10:19:23 -0700

    edite

--- EF Core test/Model/StudentSubject.cs
+++ EF Core test/Model/StudentSubject.cs
@@ -15,6 +15,7 @@ namespace EF_Core_test.Model
         public int StudentId { get; set; }
         public Student Student { get; set; }
         public int SubjectId { get; set; }
+        [Key]
         public Subject Subject { get; set; }
         public Teacher Teacher { get; set; }
         public int TeacherId { get; set; }
